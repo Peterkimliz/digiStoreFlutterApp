1c14e9a09dfdfd452f81c30c9f6af2f1c55595d5
Freddy <githumbi3fred@gmail.com>
2023-01-24 18:36:29 +0300

    finished home page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -4,9 +4,11 @@ import 'package:digi_store/controllers/product_controller.dart';
 import 'package:digi_store/models/categories.dart';
 import 'package:digi_store/models/product.dart';
 import 'package:digi_store/screens/products/components/home_product_card.dart';
+import 'package:digi_store/screens/shop/shops_page.dart';
 import 'package:digi_store/widgets/big_title.dart';
 import 'package:digi_store/widgets/small_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:shimmer/shimmer.dart';
@@ -193,8 +195,14 @@ class Home extends StatelessWidget {
                                     bigTitle(
                                         title: "Popular Shops",
                                         color: Colors.black),
-                                    smallTitle(
-                                        title: "View All", color: Colors.green)
+                                    InkWell(
+                                      onTap: () {
+                                        Get.to(() => ShopsPage());
+                                      },
+                                      child: smallTitle(
+                                          title: "View All",
+                                          color: Colors.green),
+                                    )
                                   ],
                                 ),
                               ),
@@ -212,13 +220,13 @@ class Home extends StatelessWidget {
                                         child: Stack(
                                           children: [
                                             Container(
-                                              height:180,
+                                              height: 180,
                                               width: 180,
                                               decoration: BoxDecoration(
                                                 borderRadius:
                                                     BorderRadius.circular(10),
                                                 image: DecorationImage(
-                                                  fit: BoxFit.cover,
+                                                    fit: BoxFit.cover,
                                                     image: NetworkImage(
                                                         "https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&w=800")),
                                               ),
@@ -227,7 +235,65 @@ class Home extends StatelessWidget {
                                               child: Column(
                                                 crossAxisAlignment:
                                                     CrossAxisAlignment.start,
-                                                children: [],
+                                                children: [
+                                                  Container(
+                                                      decoration: BoxDecoration(
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(3),
+                                                          color: Colors.orange),
+                                                      child: Text(
+                                                        "Open",
+                                                        style: TextStyle(
+                                                            color:
+                                                                Colors.white),
+                                                      ),
+                                                      padding:
+                                                          EdgeInsets.all(6)),
+                                                  SizedBox(height: 5),
+                                                  bigTitle(
+                                                      title: "Peter shop",
+                                                      color: Colors.white),
+                                                  SizedBox(height: 5),
+                                                  smallTitle(
+                                                      title: "Nakuru",
+                                                      color: Colors.white),
+                                                  SizedBox(height: 5),
+                                                  Row(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .spaceBetween,
+                                                    children: [
+                                                      Expanded(
+                                                          flex: 5,
+                                                          child:
+                                                              RatingBarIndicator(
+                                                            rating: 2.75,
+                                                            itemBuilder:
+                                                                (context,
+                                                                        index) =>
+                                                                    Icon(
+                                                              Icons.star,
+                                                              color:
+                                                                  Colors.amber,
+                                                            ),
+                                                            itemCount: 5,
+                                                            itemSize: 15.0,
+                                                            unratedColor:
+                                                                Colors.white,
+                                                            direction:
+                                                                Axis.horizontal,
+                                                          )),
+                                                      Expanded(
+                                                        flex: 3,
+                                                        child: smallTitle(
+                                                            title: "10 km",
+                                                            color:
+                                                                Colors.white),
+                                                      ),
+                                                    ],
+                                                  )
+                                                ],
                                               ),
                                               left: 20,
                                               right: 0,
--- lib/screens/shop/shops_page.dart
+++ lib/screens/shop/shops_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ShopsPage extends StatelessWidget {
   const ShopsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.2,
+        leading: IconButton(
+            onPressed: () => Get.back(),
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            )),
+        title: Text("All Shops", style: TextStyle(color: Colors.black)),
+      ),
+    );
   }
 }
